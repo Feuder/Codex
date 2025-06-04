c861ca7b7437c5910d1f92a7b0912f091141c61a
Feuder <caspargr@outlook.de>
2025-06-04 13:07:37 +0200

    Revert "Add navbar and organize frontend assets"

--- frontend/public/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Startseite geladen');
-  const links = document.querySelectorAll('.main-nav a');
-  links.forEach(link => {
-    link.addEventListener('click', () => {
-      console.log(`${link.textContent} geklickt`);
-    });
-  });
-});
